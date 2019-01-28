9771a710406f20be8375efc401c3194864fce76a
Hans <hans01@foxmail.com>
2019-01-28 17:58:18 +0800

    跨域

--- routers/routers.go
+++ routers/routers.go
@@ -1,10 +1,22 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	// . "roast-server/middlewares"
+	"github.com/gin-contrib/cors"
+)
+
+
 
 func InitRouter() *gin.Engine {
 
 	router := gin.Default()
+
+	/**
+	* 加载全局中间件
+	*/
+	// router.Use(Cors())	// 跨域
+	router.Use(cors.Default())
 
 	/**
 	* 加载路由	

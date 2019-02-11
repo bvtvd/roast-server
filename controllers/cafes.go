ad45f550032161ca438d7cc43f63b7131d791111
hans <hans01@foxmail.com>
2019-02-11 23:04:21 +0800

    尝试接受数组数据

--- controllers/cafes.go
+++ controllers/cafes.go
@@ -3,7 +3,7 @@ package controllers
 import (
 	"github.com/gin-gonic/gin"
 	. "roast-server/models"
-	"roast-server/utils"
+	// "roast-server/utils"
 	"fmt"
 	// "strconv"
 	// validator "gopkg.in/go-playground/validator.v9"
@@ -35,31 +35,62 @@ func CafesGetCafes(c *gin.Context) {
 	// c.String(200, "hello")
 }
 
+
+type Location struct {
+	Name string 	`form:"name"`
+	Address string `form:"address"`
+	City string `form:"city"`
+	State string  `form:"state"`
+	Zip string  `form:"zip"`
+	MethodsAvaliable []int   `form:"methodsAvaliable"`
+}
+
+type NewCafe struct {
+	Name string `form:"name"`
+	Website string `form:"website"`
+	Description string `form:"description"`
+	Roaster int `form:"roaster"`
+	Locations []Location `form:"locations"`
+}
+
 // 创建新的咖啡店
 func CafesPostNewCafe(c *gin.Context) {
-	name := c.PostForm("name")
-	address := c.PostForm("address")
-	city := c.PostForm("city")
-	state := c.PostForm("state")
-	zip := c.PostForm("zip")
+	// name := c.PostForm("name")
+	// address := c.PostForm("address")
+	// city := c.PostForm("city")
+	// state := c.PostForm("state")
+	// zip := c.PostForm("zip")
 
-	cafe := Cafe{Name: name, Address: address, City: city, State: state, Zip: zip}
-	fmt.Printf("[CAFE]: %v \n", cafe)
-	// 数据验证
-	if err := c.ShouldBind(&cafe); err != nil {
-		c.JSON(422, err.Error())
-		return
-	}
+	// cafe := Cafe{Name: name, Address: address, City: city, State: state, Zip: zip}
+	// fmt.Printf("[CAFE]: %v \n", cafe)
+	// // 数据验证
+	// if err := c.ShouldBind(&cafe); err != nil {
+	// 	c.JSON(422, err.Error())
+	// 	return
+	// }
 
-	coordinate, _ := utils.GeocodeAddress(cafe.Address, cafe.City, cafe.State)
+	// coordinate, _ := utils.GeocodeAddress(cafe.Address, cafe.City, cafe.State)
 
-	cafe.Latitude = coordinate.Lat
-	cafe.Longitude = coordinate.Lng
-	cafe.DeletedAt = nil
-	fmt.Printf("[CAFE]: %v \n", cafe)
-	PostNewCafe(&cafe)
+	// cafe.Latitude = coordinate.Lat
+	// cafe.Longitude = coordinate.Lng
+	// cafe.DeletedAt = nil
+	// fmt.Printf("[CAFE]: %v \n", cafe)
+	// PostNewCafe(&cafe)
 
-	c.JSON(201, cafe)
+	// c.JSON(201, cafe)
+
+	// name := c.PostForm("name")
+	// website := c.PostForm("website")
+	// description := c.PostForm("description")
+	// roaster := c.PostForm("roaster")
+	// locations := c.PostFormArray("locations")
+	var new NewCafe
+	c.Bind(&new)
+	// 先绑定数据进行数据验证
+	// var cafes []Cafe
+	fmt.Println("datas==========================================")
+	fmt.Printf("%v \n", new)
+
 }
 
 // 获取咖啡店详情

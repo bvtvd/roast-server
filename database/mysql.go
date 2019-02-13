4b69dcbc914e5c1cd71e9eb81768d235cd9c7d5d
Hans <hans01@foxmail.com>
2019-02-13 10:26:54 +0800

    添加配置文件

--- database/mysql.go
+++ database/mysql.go
@@ -4,6 +4,7 @@ import (
 	"github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/mysql"
     // . "roast-server/models"	// 这里的代码会造成包的循环导入
+    "os"
 )
 
 var Db *gorm.DB
@@ -11,7 +12,8 @@ var Db *gorm.DB
 func InitDb() {
 	var err error
 
-	Db, err = gorm.Open("mysql", "root:root@tcp(192.168.1.193:3306)/roast?charset=utf8&parseTime=True&loc=Local")
+	// Db, err = gorm.Open("mysql", "root:root@tcp(192.168.1.193:3306)/roast?charset=utf8&parseTime=True&loc=Local")
+	Db, err = gorm.Open("mysql", os.Getenv("MYSQL"))
 
 	if err != nil {
 		panic("database connetc error: " + err.Error())
--- main.go
+++ main.go
@@ -3,13 +3,20 @@ package main
 import (
 	. "roast-server/database"
 	"roast-server/routers"
+	"github.com/joho/godotenv"
+	"os"
 )
 
 func main() {
+	// 读取配置文件
+	if err := godotenv.Load(".env"); err != nil {
+		panic("配置文件读取错误")
+	}
+
 	// defer Db.Close()
 	InitDb()
 
 	routers := routers.InitRouter()
 
-	routers.Run(":9000")
+	routers.Run(os.Getenv("PORT"))
 }
